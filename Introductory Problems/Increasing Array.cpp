5fc376005bc907537e5c34d1b2b1400c46aa405f
Alay kabir <alaypratap23@gmail.com>
2023-09-10 20:26:07 +0530

    Update Increasing Array.cpp

--- Introductory Problems/Increasing Array.cpp
+++ Introductory Problems/Increasing Array.cpp
@@ -1,1 +1,52 @@
+#include <bits/stdc++.h>
+ 
+#define int long long
+#define endl "\n"
+#define spc " "
+#define INF_INT 2e9
+#define INF_LL 2e18
+#define matmax 25
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define pi pair<int, int>
+#define pip pair<int,pi>
+#define pl pair<ll,ll>
+#define plp pair<ll,pl>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define lb lower_bound  // First element NOT LESS than val
+#define ub upper_bound  // First element GREATER than val
+#define sz(q) (int)(q.size())
+ 
+using namespace std;
+ 
+#define fori(a,b) for(auto i=a;i<b;++i)
+#define foir(a,b) for(auto i=a;i>=b;--i)
+#define forj(a,b) for(auto j=a;j<b;++j)
+#define forjr(a,b) for(auto j=a;j>=b;--j)
 
+void solve(){
+    int n;
+    cin >> n;
+
+    vi arr(n);
+    fori(0, n) cin >> arr[i];
+
+    int ans = 0;
+    fori(1, n){
+        if(arr[i] < arr[i - 1]){
+            ans += abs(arr[i] - arr[i - 1]);
+        }
+    }
+
+    cout << ans;
+}
+
+int32_t main(){
+	fastio
+    solve();
+}
