07c0871ee132bdd6a0fca6d5949dc69648b985e7
Alay kabir <alaypratap23@gmail.com>
2023-09-10 20:14:20 +0530

    Create Repetitions.cpp

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+#define int long long
+#define endl "\n"
+#define spc " "
+#define INF_INT 2e9
+#define INF_LL 2e18
+#define matmax 25
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define pi pair<int, int>
+#define pip pair<int,pi>
+#define pl pair<ll,ll>
+#define plp pair<ll,pl>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define lb lower_bound  // First element NOT LESS than val
+#define ub upper_bound  // First element GREATER than val
+#define sz(q) (int)(q.size())
+ 
+using namespace std;
+ 
+#define fori(a,b) for(auto i=a;i<b;++i)
+#define foir(a,b) for(auto i=a;i>=b;--i)
+#define forj(a,b) for(auto j=a;j<b;++j)
+#define forjr(a,b) for(auto j=a;j>=b;--j)
+
+void solve(){
+    string s;
+    cin >> s;
+
+    int n = s.length();
+    char temp = s[0];
+    int ans = 0;
+    int maxi = 0;
+    fori(0, n){
+        if(s[i] == temp){
+            ans++;
+        }
+        else {
+            maxi = max(maxi, ans);
+            ans = 1;
+            temp = s[i];
+        }
+    }
+
+    cout << maxi;
+}
+
+int32_t main(){
+	fastio
+    solve();
+}
